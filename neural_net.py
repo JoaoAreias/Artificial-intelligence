3185d338367a4a7901746989b8ae1045c5c82ffc
JoaoAreias <joaoareiasmoraes@gmail.com>
2018-10-09 08:58:02 -0300

    Toy neural network
    Changes to be committed:
        new file:   neural_net.py

---
+++ neural_net.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+class NeuralNet:
+	"""A toy neural network for Brilliant.org
+	Neural net course"""
+	def __init__(self):
+		self.w = np.array([0, 0])
+		self.b = 0
+
+	def fit(self, data, epochs):
+		""" Receive a (x, y) data array and the number of
+		epochs, trains until convergencd or until number of
+		epochs has been reached"""
+		for epoch in range(epochs):
+			loss = 0
+			for x, y in data:
+				_y = np.matmul(self.w, x) + self.b
+				if y*_y <= 0:
+					loss += 1
+					self.w = self.w + y*x
+					self.b = self.b + y
+			print(f"Epoch {epoch}    Loss:{loss}")
+			if loss == 0:
+				break
+
+
+model =  NeuralNet()
+data = np.array([
+	(np.array([-1, 1]), 1),
+	(np.array([ 0,-1]),-1),
+	(np.array([10, 1]), 1),
+])
+model.fit(data=data, epochs=100)
+print(model.w)
+print(model.b)

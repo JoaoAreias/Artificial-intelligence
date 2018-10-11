c8058b619faaa2d45bbebee90a0f669de4c244c9
JoaoAreias <joaoareiasmoraes@gmail.com>
2018-10-11 08:10:00 -0300

    Changes to be committed: 	modified:   toy_rnn.py

--- toy_rnn.py
+++ toy_rnn.py
@@ -5,17 +5,18 @@ challanges
 import numpy as np
 
 class RNN(object):
-	Whh = np.array([[0.5, 1],[1, 2]])
-	Whx = np.array([[1, 0],[-1, 0]])
-	Why = np.array([[-1, 1],[ 1, 2]])
-	H = np.array([1, 1])
+	def __init__(self):
+		self.Whh = np.array([[0.5, 1],[1, 2]])
+		self.Whx = np.array([[1, 0],[-1, 0]])
+		self.Why = np.array([[-1, 1],[1, 2]])
+		self.H = np.array([1, 1])
 
 	def predict(self, X):
 		"""Gets an input vector X
 		and returns the prediction
 		for it"""
-		H = np.tanh(np.matmul(Whh, H) + np.matmul(Whx, X))
-		return np.matmul(Why, H)
+		self.H = np.tanh(np.matmul(self.Whh, self.H) + np.matmul(self.Whx, X))
+		return np.matmul(self.Why, self.H)
 
 
 model = RNN()
